4719b680fae4b7b4d8c105818069d2543446fdce
root <root@LAPTOP-CTP3OMHS.>
2025-03-27 11:47:14 +0800

    finish this example

---
+++ learnPETSC4/main.cpp
@@ -0,0 +1,57 @@
+static char help[] = "Solve a 4x4 linear system using KSP.\n";
+# include <petsc.h>
+
+int main(int argc, char *argv[]){
+    Vec x,b,err;
+    Mat A;
+    KSP ksp;
+    PetscInt i,j[4]={0,1,2,3}; // j=column index
+    PetscReal ab[4] = {7.0, 1.0, 1.0, 3.0}, // vector entries
+              aA[4][4] = {                  // matrix entries
+                {1.0, 2.0, 3.0, 0.0},
+                {2.0, 1.0, -2.0, -3.0},
+                {-1.0, 1.0, 1.0, 0.0},
+                {0.0, 1.0, 1.0, -1.0}
+              };
+    PetscCall(PetscInitialize(&argc,&argv,NULL,help));
+
+    PetscCall(VecCreate(PETSC_COMM_WORLD,&b));
+    PetscCall(VecSetSizes(b,PETSC_DECIDE,4));
+    PetscCall(VecSetFromOptions(b));
+    PetscCall(VecSetValues(b,4,j,ab,INSERT_VALUES));
+    PetscCall(VecAssemblyBegin(b));
+    PetscCall(VecAssemblyEnd(b));
+
+    PetscCall(MatCreate(PETSC_COMM_WORLD,&A));
+    PetscCall(MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,4,4));
+    PetscCall(MatSetFromOptions(A));
+    PetscCall(MatSetUp(A));
+    for(i=0;i<4;i++){
+        PetscCall(MatSetValues(A,1,&i,4,j,aA[i],INSERT_VALUES));
+    }
+    PetscCall(MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY));
+    PetscCall(MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY));
+
+    PetscCall(KSPCreate(PETSC_COMM_WORLD,&ksp));
+    PetscCall(KSPSetOperators(ksp,A,A));
+    PetscCall(KSPSetFromOptions(ksp));
+    PetscCall(VecDuplicate(b,&x));
+    PetscCall(KSPSolve(ksp,b,x));
+    PetscCall(VecView(x,PETSC_VIEWER_STDOUT_WORLD));
+
+    /* 计算误差*/
+    PetscCall(VecDuplicate(b,&err));
+    PetscCall(MatMult(A,x,err));
+    PetscCall(VecAXPY(err,-1.0,b));
+    PetscReal err_Norm;
+    PetscCall(VecNorm(err,NORM_2,&err_Norm)); // 计算误差的L2范数
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "L² norm of result: %g\n", (double)err_Norm));
+
+
+    PetscCall(KSPDestroy(&ksp));
+    PetscCall(MatDestroy(&A));
+    PetscCall(VecDestroy(&x));
+    PetscCall(VecDestroy(&b));
+    PetscCall(PetscFinalize());
+    return 0;
+}
--- learnPETSC4/readme.md
+++ learnPETSC4/readme.md
@@ -2,6 +2,6 @@
 线性方程组求解
 
 # 笔记
-`KSPSetOperators(ksp,A,A)` ：其中第一个 $A$ 定义了线性算子；第二个 $A$ 表示在运行时会使用 preconditioner 
-
-都设置好后，使用 `KSPSolve(ksp,b,x)` 求解线性方程组
+1. `KSPSetOperators(ksp,A,A)` ：其中第一个 $A$ 定义了线性算子；第二个 $A$ 表示在运行时会使用 preconditioner 
+2. 都设置好后，使用 `KSPSolve(ksp,b,x)` 求解线性方程组
+3. `PetscCall(VecSetValues(b,4,j,ab,INSERT_VALUES));` 中的`INSERT_VALUES`表示将提供的值直接插入到向量的指定位置，覆盖该位置原有的值。
