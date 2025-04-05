69df8f9dbb59c96e8d541f3525d744b666bcf9e9
root <root@LAPTOP-CTP3OMHS.>
2025-04-05 18:11:07 +0800

    finished, next to try debug!

--- learnPETSC6/main.cpp
+++ learnPETSC6/main.cpp
@@ -1,13 +1,72 @@
 static char help[] = "A structured-grid Possion solver using DMDA+KSP.\n.\n";
 
 # include <petsc.h>
-
+#include <unistd.h>
 extern PetscErrorCode formMatrix(DM, Mat); // 组装矩阵，PetscErrorCode数据类型，是一个整数类型，返回0表示成功，非0值表示错误
 extern PetscErrorCode formExact(DM, Vec); 
 extern PetscErrorCode formRHS(DM,Vec);
 
 int main(int argc, char* argv[]){
+    # ifdef DEBUG
+    {
+        int i=0;
+        while (0==i){
+            sleep(10);
+        }
+    }
+    #endif
+    DM da;
+    Mat A;
+    Vec b,u,uexact;
+    KSP ksp;
+    PetscReal errnorm;
+    DMDALocalInfo info;
 
+    PetscCall(PetscInitialize(&argc,&argv,NULL,help));
+
+    // change default 9x9 size using -da_grid_x M -da_grid_y N
+    PetscCall(DMDACreate2d(PETSC_COMM_WORLD,
+              DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,
+            9,9,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,&da)); // 创建二维分布式数组
+    
+    // create linear system matrix A
+    PetscCall(DMSetFromOptions(da));
+    PetscCall(DMSetUp(da));
+    PetscCall(DMCreateMatrix(da,&A));
+    PetscCall(MatSetFromOptions(A));
+
+    // create RHS b, approx solution u, exact solution uexact
+    PetscCall(DMCreateGlobalVector(da,&b));
+    PetscCall(VecDuplicate(b,&u));
+    PetscCall(VecDuplicate(b,&uexact));
+
+    // fill vectors and assemble linear system
+    PetscCall(formExact(da,uexact));
+    PetscCall(formRHS(da,b));
+    PetscCall(formMatrix(da,A));
+
+    //create and solve the linear system
+    PetscCall(KSPCreate(PETSC_COMM_WORLD,&ksp));
+    PetscCall(KSPSetOperators(ksp,A,A));
+    PetscCall(KSPSetFromOptions(ksp));
+    PetscCall(KSPSolve(ksp,b,u));
+
+    // report on grid and numerical error
+    PetscCall(VecAXPY(u,-1.0,uexact));
+    PetscCall(VecNorm(u,NORM_INFINITY,&errnorm));
+    PetscCall(DMDAGetLocalInfo(da,&info));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD,
+                          "on %d x %d grid: error |u-uexact|_inf = %g\n",
+                        info.mx,info.my,errnorm));
+    // release memory
+    PetscCall(VecDestroy(&u));
+    PetscCall(VecDestroy(&uexact));
+    PetscCall(VecDestroy(&b)); 
+    PetscCall(MatDestroy(&A));
+    PetscCall(KSPDestroy(&ksp));
+    PetscCall(DMDestroy(&da));
+    PetscCall(PetscFinalize());
+    return 0;   
 }
 
 // function definition
@@ -62,7 +121,7 @@ PetscErrorCode formExact(DM da, Vec uexact){
 
     PetscCall(DMDAGetLocalInfo(da,&info));
     hx = 1.0/(info.mx-1); hy = 1.0/(info.my-1);
-    PetscCall(DMDAVecGetArray(da, uexact, &auexact));
+    PetscCall(DMDAVecGetArray(da, uexact, &auexact)); // ??
     for (j = info.ys; j< info.ys+info.ym; j++){
         y = j * hy;
         for(i = info.xs; i<info.xs+info.xm;i++){
@@ -70,6 +129,31 @@ PetscErrorCode formExact(DM da, Vec uexact){
             auexact[j][i]=x*x*(1.0-x*x) * y*y * (y*y-1.0);
         }
     }
-    PetscCall(DMDAVecRestoreArray(da,uexact,&auexact));
+    PetscCall(DMDAVecRestoreArray(da,uexact,&auexact)); // free the data obtain from `DMDAVecGetArray`
+    return 0;
+}
+
+PetscErrorCode formRHS(DM da, Vec b){
+    PetscInt i,j;
+    PetscReal hx, hy, x, y, f, **ab;
+    DMDALocalInfo info;
+
+    PetscCall(DMDAGetLocalInfo(da,&info));
+    hx = 1.0/(info.mx-1); hy = 1.0/(info.my-1);
+    PetscCall(DMDAVecGetArray(da, b, &ab));
+    for(j=info.ys; j<info.ys+info.ym;j++){
+        y = j * hy;
+        for(i=info.xs;i<info.xs+info.xm;i++){
+            x = i * hx;
+            if (i==0 || i==info.mx-1 || j==0 || j==info.my-1){
+                ab[j][i] = 0.0; // homogenous Dirichlet boudary
+            }else {
+                f = 2.0 * ( (1.0 - 6.0*x*x) * y*y * (1.0 - y*y)
+                    + (1.0 - 6.0*y*y) * x*x * (1.0 - x*x) );
+                ab[j][i] = hx * hy * f;
+            }
+        }
+    }
+    PetscCall(DMDAVecRestoreArray(da, b, &ab));
     return 0;
 }
--- learnPETSC6/readme.md
+++ learnPETSC6/readme.md
@@ -1,6 +1,14 @@
 # 代码内容
 求解二维Possion方程
 ## 程序运行命令
+### 正常运行
+`./main -da_refine 4 -ksp_monitor -ksp_monitor_solution draw -draw_pause 0.1`
+### Debug
 
 # 笔记
-
+1. `DMDAVecGetArray(da, uexact, &auexact)` 和 `DMDAVecRestoreArray(da,uexact,&auexact)` 成对使用
+2. DMDA 是 Distributed Array (DMDA) 的缩写，DMDA 是 PETSc 的 DM（Data Management）模块的一部分，专门用于处理与网格相关的数据，特别是在求解偏微分方程（PDE）时。
+3. `DMDALocalInfo info`数据类型：
+   1. `info.mx`, `info.my`：全局网格数 (GLOBAL)
+   2. `info.xm`, `info.ym`：当前线程的局部网格数 (LOCAL)
+   3. `info.xs`, `info.ys`：当前线程的网格起点 (LOCAL)
