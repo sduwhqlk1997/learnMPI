a40cf4013054326f9013533c5fc03ea99b71ec91
root <root@LAPTOP-CTP3OMHS.>
2025-04-17 17:36:51 +0800

    unfinished

--- learnPETSC9/main.cpp
+++ learnPETSC9/main.cpp
@@ -89,12 +89,153 @@ extern PetscErrorCode Form3DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
 
 //STARTPTRARRAYS
 // arrays of pointers to functions
-/*从参考代码第92行开始*/
+static DMDASNESFunctionFn* residual_ptr[3]={
+    (DMDASNESFunctionFn*)&Poisson1DFunctionLocal,
+    (DMDASNESFunctionFn*)&Poisson2DFunctionLocal,
+    (DMDASNESFunctionFn*)&Poisson1DFunctionLocal
+};
+
+static DMDASNESJacobianFn* jacobian_ptr[3]={
+    (DMDASNESJacobianFn*)&Poisson1DJacobianLocal,
+    (DMDASNESJacobianFn*)&Poisson2DJacobianLocal,
+    (DMDASNESJacobianFn*)&Poisson3DJacobianLocal
+};
+
+typedef PetscErrorCode (*ExactFcnVec)(DMDALocalInfo*, Vec, PoissonCtx*);
+
+static ExactFcnVec getuexact_ptr[3]={&Form1DUExact,&Form2DUExact,&Form3DUExact};
+//ENDPTRARRAYS
+
+typedef enum {MANUPOLY, MANUEXP, ZERO} ProblemType;
+static const char* ProblemTypes[] = {"manupoly","manuexp","zero",
+                                    "ProblemType", "", NULL};
+// more arrays of pointers to functions:   ..._ptr[DIMS][PROBLEMS]
+typedef PetscReal (*PointwiseFcn)(PetscReal,PetscReal,PetscReal,void*);
+
+static PointwiseFcn g_bdry_ptr[3][3] = {
+    {&u_exact_1Dmanupoly, &u_exact_1Dmanuexp, &zero},
+    {&u_exact_2Dmanupoly, &u_exact_2Dmanuexp, &zero},
+    {&u_exact_3Dmanupoly, &u_exact_3Dmanuexp, &zero}
+};
+
+static PointwiseFcn f_rhs_ptr[3][3] = {
+    {&f_rhs_1Dmanupoly, &f_rhs_1Dmanuexp, &zero},
+    {&f_rhs_2Dmanupoly, &f_rhs_2Dmanuexp, &zero},
+    {&f_rhs_3Dmanupoly, &f_rhs_3Dmanuexp, &zero}
+};
+
+static const char* InitialTypes[] = {
+    "zeros","random","InitialType", "", NULL
+};
 
 int main(int argc, char* argv[]){
     DM da, da_after;
     SNES snes;
     KSP ksp;
     Vec u_initial, u, u_exact;
+    PoissonCtx user;
+    DMDALocalInfo info;
+    PetscReal errinf, normconst2h, err2h;
+    char           gridstr[99];
+    ExactFcnVec    getuexact;
+
+    // fish defaults:
+    PetscInt       dim = 2;                  // 2D
+    ProblemType    problem = MANUEXP;        // manufactured problem using exp()
+    InitialType    initial = ZEROS;          // set u=0 for initial iterate
+    PetscBool      gonboundary = PETSC_TRUE; // initial iterate has u=g on boundary
+
+    PetscCall(PetscInitialize(&argc,&argv,NULL,help));
+
+    // get options and configure context
+    user.Lx = 1.0;
+    user.Ly = 1.0;
+    user.Lz = 1.0;
+    user.cx = 1.0;
+    user.cy = 1.0;
+    user.cz = 1.0;
+    PetscOptionsBegin(PETSC_COMM_WORLD,"fsh_","options for main.cpp","");
+    PetscCall(PetscOptionsReal("-cx",
+        "set coefficient of x term u_xx in equation",
+        "main.cpp",user.cx,&user.cx,NULL));
+   PetscCall(PetscOptionsReal("-cy",
+        "set coefficient of y term u_yy in equation",
+        "main.cpp",user.cy,&user.cy,NULL));
+   PetscCall(PetscOptionsReal("-cz",
+        "set coefficient of z term u_zz in equation",
+        "main.cpp",user.cz,&user.cz,NULL));
+    PetscCall(PetscOptionsInt("-dim",
+        "dimension of problem (=1,2,3 only)",
+        "main.cpp",dim,&dim,NULL));
+    PetscCall(PetscOptionsBool("-initial_gonboundary",
+        "set initial iterate to have correct boundary values",
+        "main.cpp",gonboundary,&gonboundary,NULL));
+    PetscCall(PetscOptionsEnum("-initial_type",
+        "type of initial iterate",
+        "main.cpp",InitialTypes,(PetscEnum)initial,(PetscEnum*)&initial,NULL));
+    PetscCall(PetscOptionsReal("-Lx",
+        "set Lx in domain ([0,Lx] x [0,Ly] x [0,Lz], etc.)",
+        "main.cpp",user.Lx,&user.Lx,NULL));
+    PetscCall(PetscOptionsReal("-Ly",
+        "set Ly in domain ([0,Lx] x [0,Ly] x [0,Lz], etc.)",
+        "main.cpp",user.Ly,&user.Ly,NULL));
+    PetscCall(PetscOptionsReal("-Lz",
+        "set Ly in domain ([0,Lx] x [0,Ly] x [0,Lz], etc.)",
+        "main.cpp",user.Lz,&user.Lz,NULL));
+    PetscCall(PetscOptionsEnum("-problem",
+        "problem type; determines exact solution and RHS",
+        "main.cpp",ProblemTypes,(PetscEnum)problem,(PetscEnum*)&problem,NULL));
+    PetscOptionsEnd();
+    user.g_bdry = g_bdry_ptr[dim-1][problem];
+    user.f_rhs = f_rhs_ptr[dim-1][problem];
+
+    if ( user.cx <= 0.0 || user.cy <= 0.0 || user.cz <= 0.0 ) {
+        SETERRQ(PETSC_COMM_SELF,2,"positivity required for coefficients cx,cy,cz\n"); // 终止程序运行，PETSC_COMM_SELF表示当前进程
+    }
+    if ((problem == MANUEXP) && ( user.cx != 1.0 || user.cy != 1.0 || user.cz != 1.0)) {
+        SETERRQ(PETSC_COMM_SELF,3,"cx=cy=cz=1 required for problem MANUEXP\n");
+    }
+    //STARTCREATE
+    //create DMDA in chosen dimension
+    switch (dim){
+        case 1:
+            PetscCall(DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,3,1,1,NULL,&da));
+            break;
+        case 2:
+            PetscCall(DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,3,3,
+            PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,&da));
+            break;
+        case 3:
+            PetscCall(DMDACreate3d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
+            DMDA_STENCIL_STAR,3,3,3,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,
+            1,1,NULL,NULL,NULL,&da));
+            break;
+        default:
+            SETERRQ(PETSC_COMM_SELF,1,"invalid dim for DMDA creation\n");
+    }
+    PetscCall(DMSetApplicationContext(da,&user));
+    PetscCall(DMSetFromOptions(da));
+    PetscCall(DMSetUp(da)); // call BEFORE SetUniformCoordinates
+    PetscCall(DMDASetUniformCoordinates(da,0.0,user.Lx,0.0,user.Ly,0.0,user.Lz)); // 均匀分布的网格
+
+    // set SNES call-backs
+    PetscCall(SNESCreate(PETSC_COMM_WORLD,&snes));
+    PetscCall(SNESSetDM(snes,da));
+    PetscCall(DMDASNESSetFunctionLocal(da,INSERT_VALUES,(DMDASNESFunctionFn *)(residual_ptr[dim-1]),&user));
+    PetscCall(DMDASNESSetJacobianLocal(da,(DMDASNESJacobianFn *)(jacobian_ptr[dim-1]),&user));
+
+    // default to KSPONLY+CG because problem is linear and SPD
+    PetscCall(SNESSetType(snes,SNESKSPONLY)); // 表示仅使用 KSP（Krylov Subspace Solver）求解线性系统。
+    PetscCall(SNESGetKSP(snes,&ksp));
+    PetscCall(KSPSetType(ksp,KSPCG));
+    PetscCall(SNESSetFromOptions(snes));
+
+    // set initial iterate and then solve
+    PetscCall(DMGetGlobalVector(da,&u_initial));
+    PetscCall(InitialState(da, initial, gonboundary, u_initial, &user));
+    PetscCall(SNESSolve(snes,NULL,u_initial));
+    //ENDCREATE
+
+    //STARTGETSOLUTION (242 行)
     return 0;
 }
