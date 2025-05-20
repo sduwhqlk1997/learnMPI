7f8d4e64b625f7ad55c5e129305e4bdc5bb64e77
root <root@LAPTOP-CTP3OMHS.>
2025-05-20 12:07:23 +0800

    assemble Mat & Vec finished

--- learnPETSC15/source/poissonFEM.cpp
+++ learnPETSC15/source/poissonFEM.cpp
@@ -27,12 +27,13 @@ static PetscBool Isbound(PetscInt i, PetscInt j, const DMDALocalInfo* info){
 PetscErrorCode formMatrixVec(DM da, Mat A, Vec b, PoissonCtx *user){
     DMDALocalInfo info;
     const PetscInt li[4] = {0,1,1,0}, lj[4] = {0,0,1,1};
-    PetscReal hx,hy,x,y,kx,ky,tx,ty, xymin[2], xymax[2], xEleStart, yEleStart;
+    PetscReal hx,hy,x,y,kx,ky,tx,ty,cx,cy, xymin[2], xymax[2], xEleStart, yEleStart;
     PetscCall(DMDAGetLocalInfo(da,&info));
     PetscCall(DMGetBoundingBox(info.da,xymin,xymax));
 
     hx = user->Lx/(info.mx-1); hy = user->Ly/(info.my-1);
     ky = hy / 2.0; kx = hx / 2.0;
+    cx = 4/(hx*hx); cy = 4/(hy*hy);
 
     MatStencil* AIndexInsert = new MatStencil[info.mx*info.my];
     PetscReal* AValueInsert = new PetscReal[info.mx*info.my];
@@ -70,24 +71,53 @@ PetscErrorCode formMatrixVec(DM da, Mat A, Vec b, PoissonCtx *user){
                     bValueInsert[bNInsert++] = user->g_Dir(x,y);
                 }else{
                     // 向量 此时测试函数为内部点，做积分
-                    Fun2D IntFun=[l,kx,ky,tx,ty,user](PetscReal x, PetscReal y){
+                    Fun2D IntFunVec=[l,kx,ky,tx,ty,user](PetscReal x, PetscReal y){
                         return chi(l,x,{0,0},y) * user->f_rhs(kx*x+tx,ky*y+ty);
                     };
                     bindexAdd[bNAdd] = info.mx*jtest+itest;
-                    bValueAdd[bNAdd++] = GaussIntegral(IntFun, hx, hy, user->quadpts);
+                    bValueAdd[bNAdd++] = GaussIntegral(IntFunVec, hx, hy, user->quadpts);
 
-                    for(PetscInt r=0; r<4; r++){//试探函数(列)/************未完成**************/
+                    for(PetscInt r=0; r<4; r++){//试探函数(列)
                         PetscInt itrail = i+li[r], jtrail=j+lj[r];
+                        Fun2D IntFunMat = [l,r,cx,cy](PetscReal x,PetscReal y){
+                                return chi(l,x,{1,0},y)*chi(r,x,{1,0},y)*cx+
+                                chi(l,x,{0,1},y)*chi(r,x,{0,1},y)*cy;
+                        };
                         if(Isbound(itrail,jtrail,&info)==PETSC_TRUE){//边界点对应的列
-
+                            x = xymin[0] + itrail*hx; y = xymin[1] + jtrail*hy;
+                            PetscReal boundVal = user->g_Dir(x,y);
+                            bindexAdd[bNAdd] = info.mx*jtest + itest;
+                            bValueAdd[bNAdd++] = -boundVal*GaussIntegral(IntFunMat,hx,hy,user->quadpts);
                         }else{//test和trail全为内部点
-
+                            ArowAdd[ANAdd].c=0; ArowAdd[ANAdd].k=0; ArowAdd[ANAdd].i=itest; ArowAdd[ANAdd].j=jtest;
+                            AcolAdd[ANAdd].c=0; AcolAdd[ANAdd].k=0; AcolAdd[ANAdd].i=itrail; AcolAdd[ANAdd].j=jtrail;
+                            AValueAdd[ANAdd++] = GaussIntegral(IntFunMat,hx,hy,user->quadpts);
                         }
                     }
                 }
             }
         }
     }
+    // 向矩阵和向量中填充值
+    for(PetscInt i=0;i<ANAdd;i++){
+        PetscCall(MatSetValuesStencil(A,1,&ArowAdd[i],1,&AcolAdd[i],&AValueAdd[i],ADD_VALUES));
+    }
+    PetscCall(MatAssemblyBegin(A,MAT_FLUSH_ASSEMBLY));
+    PetscCall(MatAssemblyEnd(A,MAT_FLUSH_ASSEMBLY));
+
+    for(PetscInt i=0;i<ANInsert;i++){
+        PetscCall(MatSetValuesStencil(A,1,&AIndexInsert[i],1,&AIndexInsert[i],&AValueInsert[i],INSERT_VALUES));
+    }
+    PetscCall(MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY));
+    PetscCall(MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY));
+
+    PetscCall(VecSetValues(b,bNAdd,bindexAdd,bValueAdd,ADD_VALUES));
+    PetscCall(VecAssemblyBegin(b));
+    PetscCall(VecAssemblyEnd(b));
+
+    PetscCall(VecSetValues(b,bNInsert,bindexInsert,bValueInsert,INSERT_VALUES));
+    PetscCall(VecAssemblyBegin(b));
+    PetscCall(VecAssemblyEnd(b));
     return PETSC_SUCCESS;
 }
 
